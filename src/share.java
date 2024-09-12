99ada40a10eb68e148c2d47e793096895e2cd0d7
Chewbacca <higgimat000@warren.k12.in.us>
2024-09-12 13:56:19 -0400

    initial commit

--- src/share.java
+++ src/share.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class share {
+public class share {
+    public static void main(String[] args) {
+        System.out.println(hasSharedDigit(12,23));
+        System.out.println(hasSharedDigit(9, 99));
+        System.out.println(hasSharedDigit(12, 34));
+    }
+
+    public static boolean hasSharedDigit(int first, int second) {
+        if(first < 10 || first > 99){
+            return false;
+        }
+        if(second < 10 || second > 99){
+            return false;
+        }
+
+        int secondCopy = second;
+
+        while (first > 0){
+            //Get the last digit of first with mod store in firstCheck
+            int firstCheck = first % 10;
+            while(second > 0){
+                //Get the last digit of second with mod store in secCheck
+                int secCheck = second % 10;
+                if(firstCheck == secCheck){
+                    return true;
+                }
+                second /= 10;
+            }
+            first /= 10;
+            second = secondCopy;
+        }
+
+        return false;
+
+    }
 }
